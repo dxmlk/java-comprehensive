c55e88ce04085d9d1ba1f44b129355e79f9d5f88
dxmlk <1101sky@korea.ac.kr>
2025-03-06 14:31:35 +0900

    feat: ScheduleService의 Schedule 추가, 삭제,  조회

--- src/main/java/com/dxmlk/service/ScheduleService.java
+++ src/main/java/com/dxmlk/service/ScheduleService.java
@@ -1,4 +1,30 @@
 package com.dxmlk.service;
 
+import com.dxmlk.domain.Schedule;
+import com.dxmlk.persistence.ScheduleStorage;
+
+import java.util.List;
+
 public class ScheduleService {
+    private final ScheduleStorage scheduleStorage;
+
+    public ScheduleService(ScheduleStorage scheduleStorage) {
+        this.scheduleStorage = scheduleStorage;
+    }
+
+    public void addSchedule(Schedule schedule) {
+        scheduleStorage.addSchedule(schedule);
+    }
+
+    public void removeSchedule(int studentId) {
+        scheduleStorage.removeSchedule(studentId);
+    }
+
+    public Schedule getScheduleByStudentId(int studentId) {
+        return scheduleStorage.getScheduleByStudentId(studentId);
+    }
+
+    public List<Schedule> getAllSchedules() {
+        return scheduleStorage.getAllSchedules();
+    }
 }
