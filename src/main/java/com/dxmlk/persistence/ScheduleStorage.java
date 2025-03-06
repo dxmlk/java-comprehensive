8d46b83999467bd165a1d9dfe94d8aaf5f3bf8fc
dxmlk <1101sky@korea.ac.kr>
2025-03-06 14:23:47 +0900

    feat: ScheduleStorage에서 Schedule 정보 저장 및 불러오기

--- src/main/java/com/dxmlk/persistence/ScheduleStorage.java
+++ src/main/java/com/dxmlk/persistence/ScheduleStorage.java
@@ -1,4 +1,33 @@
 package com.dxmlk.persistence;
 
+import com.dxmlk.domain.Schedule;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScheduleStorage {
+    private static final String FILE_NAME = "scheduleDB.dat";
+
+    public void saveSchedules(List<Schedule> schedules) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(schedules);
+        } catch (IOException e) {
+            System.err.println("시간표 데이터를 저장하는 중 오류 발생: " + e.getMessage());
+        }
+    }
+
+    public List<Schedule> loadSchedules() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (List<Schedule>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("시간표 데이터를 불러오는 중 오류 발생: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
 }
