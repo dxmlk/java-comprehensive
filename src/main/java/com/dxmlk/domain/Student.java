5247f1af20b53ec423257212d73bb3b71b43f5ba
dxmlk <1101sky@korea.ac.kr>
2025-03-06 14:20:07 +0900

    feat: Student 객체, 생성자, getter 및 setter, toString()

--- src/main/java/com/dxmlk/domain/Student.java
+++ src/main/java/com/dxmlk/domain/Student.java
@@ -1,4 +1,53 @@
 package com.dxmlk.domain;
 
-public class Student {
+import java.io.Serializable;
+import java.util.List;
+
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String studentId;
+    private String name;
+    private int year;
+    private List<Schedule> scheduleList; // 수강 신청된 강의 목록
+
+    public Student(String studentId, String name, int year, List<Schedule> scheduleList) {
+        this.studentId = studentId;
+        this.name = name;
+        this.year = year;
+        this.scheduleList = scheduleList;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<Schedule> getScheduleList() {
+        return scheduleList;
+    }
+
+    public void addCourse(Schedule schedule) {
+        scheduleList.add(schedule);
+    }
+
+    public void removeCourse(Schedule schedule) {
+        scheduleList.remove(schedule);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                ", scheduleList=" + scheduleList +
+                '}';
+    }
 }
