0e7cc1e9c2f7f5c912a7ee610c99f4d1b43762be
dxmlk <1101sky@korea.ac.kr>
2025-03-06 14:35:54 +0900

    feat: StudentService의 Student 추가, 삭제, 조회 기능 추가

--- src/main/java/com/dxmlk/service/StudentService.java
+++ src/main/java/com/dxmlk/service/StudentService.java
@@ -1,4 +1,38 @@
 package com.dxmlk.service;
 
+import com.dxmlk.domain.Student;
+import com.dxmlk.persistence.StudentStorage;
+
+import java.util.List;
+import java.util.Optional;
+
 public class StudentService {
+    private final StudentStorage studentStorage;
+
+    public StudentService() {
+        this.studentStorage = new StudentStorage();
+    }
+
+    public void addStudent(Student student) {
+        List<Student> students = studentStorage.loadStudents();
+        students.add(student);
+        studentStorage.saveStudents(students);
+    }
+
+    public void removeStudent(int studentId) {
+        List<Student> students = studentStorage.loadStudents();
+        students.removeIf(student -> student.getStudentId().equals(studentId));
+        studentStorage.saveStudents(students);
+    }
+
+    public Optional<Student> getStudentById(int studentId) {
+        List<Student> students = studentStorage.loadStudents();
+        return students.stream()
+                .filter(student -> student.getStudentId().equals(studentId))
+                .findFirst();
+    }
+
+    public List<Student> getAllStudents() {
+        return studentStorage.loadStudents();
+    }
 }
