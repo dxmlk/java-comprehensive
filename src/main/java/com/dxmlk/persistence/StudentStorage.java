26ab8adcefd97f12e4cb4b4dd2be88891f158293
dxmlk <1101sky@korea.ac.kr>
2025-03-06 14:23:03 +0900

    feat: StudentStorage에서 Student 정보 저장 및 불러오기

--- src/main/java/com/dxmlk/persistence/StudentStorage.java
+++ src/main/java/com/dxmlk/persistence/StudentStorage.java
@@ -1,4 +1,33 @@
 package com.dxmlk.persistence;
 
+import com.dxmlk.domain.Student;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentStorage {
+    private static final String FILE_NAME = "studentDB.dat";
+
+    public void saveStudents(List<Student> students) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Student> loadStudents() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (List<Student>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 }
